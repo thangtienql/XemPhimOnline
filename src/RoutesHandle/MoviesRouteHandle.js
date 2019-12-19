b1c8106c3c15bf64e0858da5cce12d2128c4cc72
PhucHoang <lzootee@gmail.com>
2019-12-19 22:17:53 +0700

    pagination

--- src/RoutesHandle/MoviesRouteHandle.js
+++ src/RoutesHandle/MoviesRouteHandle.js
@@ -1,24 +1,27 @@
 
-const ApiGetData= require('../ApiServices/ApiGetData.js');
+const ApiGetData = require('../ApiServices/ApiGetData.js');
 
-const GetMoviesByCategoryRoute = async (req, res, next)=>{
-    let data={};
+const GetMoviesByCategoryRoute = async (req, res, next) => {
+    let data = {};
     try {
-       const ress=await ApiGetData.getMoviesByCateGory();
-       console.log(ress)
-        const { response :{
-            data:listMovies
-        }}=ress;
-        // res.render('index', { title: 'Express', dataMovieHot: dataMovieHot, dataNewMovies: dataNewMovies})
-        res.render('list', { listMovies:listMovies})
-    } catch(err) {
-        console.log('error:',err)
+        let page = req.query.page ? parseInt(req.query.page) : 1;
+        let perPage = 12;
+        let categoryId = req.query.cat;
+        const ress = await ApiGetData.getMoviesByCateGory(categoryId, page, perPage);
+        const { response: {
+            data: listMovies
+        } } = ress;
+        console.log(ress);
+        let lastPage = Math.ceil(ress.total_movie/perPage);
+        res.render('list', { listMovies: listMovies, page: page, lastPage: lastPage });
+    } catch (err) {
+        console.log('error:', err)
         res.render('index', {
             title: 'Xem Phim',
         })
     }
 }
 
-module.exports ={
+module.exports = {
     GetMoviesByCategoryRoute
 }
