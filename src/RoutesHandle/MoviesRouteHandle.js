1a88125b436620a5e0ce094f657f497a0ca3766f
macbook <haihoang1993@gmail.com>
2019-12-25 21:29:46 +0700

    add code

--- src/RoutesHandle/MoviesRouteHandle.js
+++ src/RoutesHandle/MoviesRouteHandle.js
@@ -5,16 +5,19 @@ const { CategoryDB } = require('../Database/SchemaDB');
 const GetMoviesByCategoryRoute = async (req, res, next) => {
     let data = {};
     try {
+
         let page = req.query.page ? parseInt(req.query.page) : 1;
         let perPage = 12;
         let slugCategory = req.params['slug'];
-        let category = await CategoryDB.findOne({cat_name_link: slugCategory});
+        let category = await CategoryDB.findOne({ cat_name_link: slugCategory });
         const ress = await ApiGetData.getMoviesByCateGory(category.cat_id, page, perPage);
+        const {cat_name=''}=category;
         const { response: {
             data: listMovies
         } } = ress;
-        let lastPage = Math.ceil(ress.total_movie/perPage);
-        res.render('list', { listMovies: listMovies, page: page, lastPage: lastPage });
+
+        let lastPage = Math.ceil(ress.total_movie / perPage);
+        res.render('list', { title:cat_name,listMovies: listMovies, page: page, lastPage: lastPage });
     } catch (err) {
         console.log('error:', err)
         res.render('index', {
